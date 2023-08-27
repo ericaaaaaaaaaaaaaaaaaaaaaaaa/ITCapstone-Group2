66fed78bfad2deda0f95cd6e0b7d8d87dd970442
Rachel-ValenciaCapstone <143286612+Rachel-ValenciaCapstone@users.noreply.github.com>
2023-08-27 19:36:50 -0400

    Update userstable.sql

--- userstable.sql
+++ userstable.sql
@@ -2,6 +2,5 @@ CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    profile_picture VARCHAR(255)
+    password VARCHAR(255) NOT NULL
 );
