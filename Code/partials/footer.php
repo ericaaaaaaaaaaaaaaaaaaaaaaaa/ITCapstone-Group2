6dd9b366f07a3c5810939a998bc5f90944fc4589
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-09-22 14:54:01 -0400

    Create footer.php

---
+++ Code/partials/footer.php
@@ -0,0 +1,50 @@
+<div class="col">
+    <img class="logo" src="img/logo.png" alt="logo">
+    <h4>Contact</h4>
+    <p><strong>Address:</strong> 5678 Sunset Boulevard, Los Angeles, CA 90001</p>
+    <p><strong>Phone:</strong> (555) 123-4567</p>
+    <p><strong>Hours:</strong> 9:00 AM - 5:00 PM, Mon-Sat</p>
+    <div class="follow">
+        <h4>Follow Us</h4>
+        <div class="icon">
+            <i class="fa fa-facebook"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-instagram"></i>
+            <i class="fa fa-youtube-play"></i>
+        </div>
+    </div>
+</div>
+<div class="col">
+    <h4>About</h4>
+    <a href="#">About Us</a>
+    <a href="#">Delivery Information</a>
+    <a href="#">Privacy Policy</a>
+    <a href="#">Terms & Conditions</a>
+    <a href="#">Contact Us</a>
+</div>
+
+<div class="col">
+    <h4>My Account</h4>
+    <a href="#">Sign In</a>
+    <a href="#">View Cart</a>
+    <a href="#">My Wishlist</a>
+    <a href="#">Track My Order</a>
+    <a href="#">Help</a>
+</div>
+
+<div class="col install">
+    <h4>Install App</h4>
+    <p>From App Store or Google Play</p>
+    <div class="row">
+        <img src="img/pay/app.jpg" alt="App Store">
+        <img src="img/pay/play.jpg" alt="Google Play">
+    </div>
+    <p>Secured Payment Gateways</p>
+    <img src="img/pay/pay.png" alt="Payment Methods">
+</div>
+
+<div class="copyright">
+    <p>© 2023, IT Capstone Group 2 - Women in Stem</p>
+</div>
+
+<button onclick="topFunction()" id="pgup" class="material-symbols-rounded">arrow_circle_up</button>
