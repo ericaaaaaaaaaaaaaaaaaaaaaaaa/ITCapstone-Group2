30ca6bb140bf2fca2e2edeca4c783caeea20718f
ericaaaaaaaaaaaaaaaaaaaaaaaa <144266425+ericaaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2023-09-22 14:53:43 -0400

    Create login.php

---
+++ Code/partials/login.php
@@ -0,0 +1,47 @@
+<div class="form_container">
+    <i class="material-symbols-rounded form_close">close</i>
+    <div class="form login_form">
+        <form action="#">
+            <h2>Login</h2>
+            <div class="input_box">
+                <input type="email" placeholder="Enter you email" required /><i class="material-symbols-rounded email">mail</i>
+            </div>
+            <div class="input_box">
+                <input type="password" placeholder="Enter you password" required /><i class="material-symbols-rounded password">lock</i>
+                <i class="fa fa-eye-slash pw_hide"></i>
+            </div>
+            <div class="option_field">
+                <span class="checkbox">
+                    <input type="checkbox" id="check">
+                    <label for="checkbox">Remember me</label>
+                </span>
+                <a href="#" class="forgot_pw">Forgot password?</a>
+            </div>
+            <button class="button">Login Now</button>
+            <div class="login_signup">
+                Don't have an account? <a href="#" id="signup">Signup</a>
+            </div>
+        </form>
+    </div>
+
+    <div class="form signup_form">
+        <form action="#">
+            <h2>Signup</h2>
+            <div class="input_box">
+                <input type="email" placeholder="Enter you email" required /><i class="material-symbols-rounded email">mail</i>
+            </div>
+            <div class="input_box">
+                <input type="password" placeholder="Create password" required /><i class="material-symbols-rounded password">lock</i>
+                <i class="fa fa-eye-slash pw_hide"></i>
+            </div>
+            <div class="input_box">
+                <input type="password" placeholder="Confirm password" required /><i class="material-symbols-rounded password">lock</i>
+                <i class="fa fa-eye-slash pw_hide"></i>
+            </div>
+            <button class="button">Signup Now</button>
+            <div class="login_signup">
+                Already have an account? <a href="#" id="login">Login</a>
+            </div>
+        </form>
+    </div>
+</div>
